8481db7f1ae5c13c48b9ea839c2d1d958bade8c3
wesley-tang <wesleytang1@gmail.com>
2018-11-30 01:48:33 -0700

    Began scheduling algorithm

---
+++ Overlap/app/src/main/java/overlap/project/scheduler/Scheduler.java
@@ -0,0 +1,29 @@
+package overlap.project.scheduler;
+
+import java.util.ArrayList;
+
+/**
+ * Scheduler manages the scheduling of events.
+ *
+ */
+public class Scheduler {
+
+	private ArrayList<User> users = new ArrayList<>();
+
+	/**
+	 * Creates a new scheduler based off the given
+	 *
+	 * @param ArrayList<User> to be considered for the scheduling.
+	 */
+	public Scheduler(ArrayList<User> users){
+		this.users = users;
+	}
+
+	/**
+	 *
+	 */
+	public void schedule(){
+
+	}
+
+}
---
+++ Overlap/app/src/main/java/overlap/project/scheduler/User.java
@@ -0,0 +1,19 @@
+package overlap.project.scheduler;
+
+/**
+ * Object that aggregates properties of a user.
+ */
+public class User {
+	// ICS file of user
+	//private ics
+
+	// Preferences of user
+	//private preferences
+
+	public User(){
+
+	}
+
+	// GETTERS ~~~~~
+
+}
