abc3b1b432b78804f6c7af454e61ef6ceffb5ac5
wesley-tang <wesleytang1@gmail.com>
2018-12-02 20:31:26 -0700

    Updated DAteRange with contains and more

--- Overlap/app/src/main/java/overlap/project/scheduler/DateRange.java
+++ Overlap/app/src/main/java/overlap/project/scheduler/DateRange.java
@@ -2,6 +2,7 @@ package overlap.project.scheduler;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
@@ -22,45 +23,68 @@ public class DateRange {
 	// The pattern by which to convert iCalnedar times to Date objects
 	private final String datePattern = "yyyyMd'T'Hms";
 
-	// Check this flag and do not use if false, meaning dates failed to parse and are null or incorrect.
-	private boolean correctlyParsed;
-
 	/**
 	 * Creates a new DateRange from the given start and end. Must be written in iCalendar format
 	 *
 	 * @param start String containing the beginning of range in iCalendar format.
 	 * @param end String containing the end of range in iCalendar format.
+	 *
+	 * @throws ParseException if the format given by the parameters is not valid iCalendar format
 	 */
-	public DateRange(String start, String end){
+	public DateRange(String start, String end) throws ParseException {
 		this.start = start;
 		this.end = end;
 
-		// Obtains the start and end dates from the provided string, while tracking if conversion was successful
-		correctlyParsed = convertToDates();
+		// Obtains the start and end dates from the provided string
+		convertToDates();
 	}
 
 	// Attempts to parse the iCalendar strings to produce Date objects, returning whether it was successful
-	private boolean convertToDates() {
+	private void convertToDates() throws ParseException {
 		// Define the date format used by the iCalander standard (no timezone)
 		SimpleDateFormat icsFormat = new SimpleDateFormat(datePattern, Locale.CANADA);
 
 		// Convert the strings to dates.
-		try {
-			startDate = icsFormat.parse(start);
-		} catch (ParseException e) {
-			System.out.println("Failed to parse: " + start);
-			e.printStackTrace();
-			return false;
-		}
-		try {
-			endDate = icsFormat.parse(end);
-		} catch (ParseException e) {
-			System.out.println("Failed to parse: " + end);
-			e.printStackTrace();
-			return false;
+		startDate = icsFormat.parse(start);
+		endDate = icsFormat.parse(end);
+	}
+
+	/**
+	 * @param toCheck the Date to check if it's in the date range
+	 * @return true if the passed Date is contained within the invoking DateRange.
+	 */
+	public boolean contains(Date toCheck){
+		return toCheck.after(startDate) && toCheck.before(endDate);
+	}
+
+	/**
+	 * @param toCheck the DateRange to check if it's in the date range
+	 * @return true if the passed DateRange is contained within the invoking DateRange.
+	 */
+	public boolean contains(DateRange toCheck){
+		return toCheck.getStart().after(startDate) && toCheck.getEnd().before(endDate);
+	}
+
+	/**
+	 * Returns a copy of the passed list of date ranges with the
+	 * invoking DateRange removed from the specified date ranges..
+	 *
+	 * @param dates
+	 */
+	public ArrayList<DateRange> removeFrom(ArrayList<DateRange> dates){
+		ArrayList<DateRange> newDates;
+
+		for (DateRange dateRange : dates){
+			// If the date range given does not contain this daterange, try the next one
+			if (!dateRange.contains(this)){
+				continue;
+			}
+			else {
+
+			}
 		}
 
-		return true;
+		return dates;
 	}
 
 	/**
--- Overlap/app/src/main/java/overlap/project/scheduler/Scheduler.java
+++ Overlap/app/src/main/java/overlap/project/scheduler/Scheduler.java
@@ -1,8 +1,7 @@
 package overlap.project.scheduler;
 
-import java.text.SimpleDateFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Locale;
 
 /**
  * Scheduler manages the scheduling of events.
@@ -23,15 +22,31 @@ public class Scheduler {
 
 	/**
 	 *
+	 *
 	 * @param validDays an ArrayList of Strings containing the
 	 */
-	public void schedule(ArrayList<DateRange> validDays){
+	public DateRange schedule(ArrayList<DateRange> validDays){
+
+		String start = "";
+		String end = "";
+
+		
+		// Apply and filter out restrictions
+		// Obtain all data ranges capable of cotnaining an event
+		// Break range into 'slots' capable of contaiing the event and choose one
 
 
 
+		DateRange scheduledEvent = null;
+		try {
+			scheduledEvent = new DateRange(start, end);
+		} catch (ParseException e) {
+			//TODO  Alert user with notification
+			System.out.println("Could not create schdeule because parse failed!");
+			e.printStackTrace();
+		}
 
-
-
+		return scheduledEvent;
 	}
 
 
