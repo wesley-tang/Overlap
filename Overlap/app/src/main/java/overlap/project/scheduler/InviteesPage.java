7f92838dc136a1754e42033f88b72829aba0b44c
Greyson <greysonleighm@gmail.com>
2018-12-06 10:38:49 -0700

    Added all other modules to settings page + a fuckton more coded strings, started invitee page activity

---
+++ Overlap/app/src/main/java/overlap/project/scheduler/InviteesPage.java
@@ -0,0 +1,15 @@
+package overlap.project.scheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import overlap.project.R;
+
+public class InviteesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invitees_page);
+    }
+}
